9a467085b71ef831678d953fa4fbd2984a0eba18
shgh2233 <shgh2233@gmail.com>
2020-07-14 22:38:28 +0900

    Card, Hint, Action 클래스 설계 및 오류 수정
    - 이미 생성자가 있는 상태에서 다른 자료형의 매개변수를 받는 생성자를 선언 할 수 없어(덮어 씌워짐) 한 생성자 내에서 모두 처리하도록 클래스 생성자 수정
    - 자잘한 오류 수정
    - 기능 테스트 완료

--- Game/HanabiGame.py
+++ Game/HanabiGame.py
@@ -1,27 +1,62 @@
+class Hint:
+
+    def __init__(self, info):
+        """
+        :param info: 색과 숫자 힌트. 문자열 혹은 숫자를 받을 수 있도록 자료형을 명시하지 않았음
+        """
+        assert info == "R" or info == "G" or info == "B" or info == "W" or info == "Y" \
+            or info == 1 or info == 2 or info == 3 or info == 4 or info == 5, "invalid card information"
+
+        self.info = info
+
+    def isNumber(self):
+        return type(self.info) == int
+
+    def isColor(self):
+        return type(self.info) == str
+
+
+class Card:
+
+    def __init__(self, color: str, number: int):
+        """
+        :param color: 카드의 색
+        :param number: 카드의 숫자
+        """
+        assert color == "R" or color == "G" or color == "B" or color == "W" or color == "Y", "invalid card color"
+        assert 1 <= number <= 5, "invalid card number"
+        self.__color = color
+        self.__number = number
+
+    def getColor(self):
+        return self.__color
+
+    def getNumber(self):
+        return self.__number
+
+    def isCorrespondedHint(self, hint: Hint):
+        return self.__color == hint.info or self.__number == hint.info
+
+
 class Action:
 
-    def __init__(self, actionType: int, playerIndex: int, cardIndex: int):
+    def __init__(self, actionType: int, playerIndex: int, element):
         """
-        액션 타입 1,2에 대응하는 생성자
         :param actionType: Action의 타입을 의미 1-내려놓기 / 2-버리기 / 3-힌트주기
-        :param playerIndex: Action을 수행하는 플레이어의 색인
-        :param cardIndex: 내고자 하는 카드의 색인
+        :param playerIndex: Action을 수행하는 플레이어의 색인(1,2) / 힌트를 받을 플레이어의 색인(3)
+        :param element: 내고자 하는 카드의 색인(1,2 cardIndex: int), 힌트 객체(3 hint: Hint)
         """
+        assert 1 <= actionType <= 3, "invalid action type"
 
         self.__actionType = actionType
         self.__playerIndex = playerIndex
-        self.__hint = cardIndex
-
-    def __init__(self, actionType: int, playerIndex: int, hint: Hint):
-        """
-        액션 타입 3에 대응하는 생성자
-        :param actionType: Action의 타입을 의미 1-내려놓기 / 2-버리기 / 3-힌트주기
-        :param playerIndex: 힌트를 받을 플레이어의 색인
-        :param hint: 힌트 객체
-        """
-        self.__actionType = actionType
-        self.__playerIndex = playerIndex
-        self.__hint = hint
+        if actionType == 3:
+            assert type(element) == Hint, "invalid element. Element should be Hint class when actionType is 3"
+            self.__hint = element
+        else:
+            assert type(element) == int, "invalid element.Element should be integer when actionType is 1or2"
+            assert 0 <= element <= 3, "invalid card index."
+            self.__cardIndex = element
 
     def getActionType(self):
         return self.__actionType
@@ -30,37 +65,57 @@ class Action:
         return self.__playerIndex
 
     def getHint(self):
+        # getActionType()을 통해 메소드를 호출 할 수 있는지 확인하고 호출 할 것
+        assert self.__actionType == 3
         return self.__hint
 
     def getCardIndex(self):
-        return self.getCardIndex()
+        # getActionType()을 통해 메소드를 호출 할 수 있는지 확인하고 호출 할 것
+        assert self.__actionType != 3
+        return self.__cardIndex
 
-class Card:
 
-    def __init__(self, cardColor: str, cardNumber: int):
-        """
-        카드 색과 숫자에 대응하는 생산자
-        :param cardColor: 카드의 색
-        :param cardNumber: 카드의 숫자
-        """
-        self.__cardColor = cardColor
-        self.__cardNumber = cardNumber
+#     기능 테스트      #
+def test___():
+    h = Hint("R")
+    h = Hint("G")
+    h = Hint("B")
+    h = Hint("W")
+    h = Hint("Y")
+    assert not h.isNumber()
+    assert h.isColor()
+    h = Hint(1)
+    h = Hint(2)
+    h = Hint(3)
+    h = Hint(4)
+    h = Hint(5)
+    assert h.isNumber()
+    assert not h.isColor()
+    # h = Hint("K")
+    # h = Hint(6)
+    # h = Hint(0)
 
-    def getColor(self):
-        return self.__cardColor
+    colors = ["R", "G", "B", "W", "Y"]
 
-    def getNumber(self):
-        return self.__cardNumber
+    for i in range(5):
+        for color in colors:
+            c = Card(color, i + 1)
 
-    def isCorrespondedHint(self, hint):
-        return self.__cardColor == hint.info or self.__cardNumber == int(hint.info)
+    # c = Card("J", 1)
+    # c = Card("R", 6)
 
-class Hint:
+    for i in range(1, 3):
+        for j in range(5):
+            for k in range(4):
+                a = Action(i, j, k)
+    assert a.getCardIndex() == 3
+    # a.getHint()
 
-    def __init__(self, hint: str):
-        """
-        힌트에 대응하는 생산자
-        :param hint: 주고자하는 힌트
-        """
-        self.info = info
-        # test comment
+    for j in range(5):
+        a = Action(3, j, h)
+    # a.getCardIndex()
+    assert a.getHint().info == 5
+
+    # a = Action(3, 3, 4)
+
+    print("Test Over")
