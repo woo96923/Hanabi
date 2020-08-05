d59384a1d765b9e55129677eaca2b908597b94c2
woo96923 <woo96923@gmail.com>
2020-08-05 14:08:48 +0900

    동기화

--- GUI/HanabiAlpha01.py
+++ GUI/HanabiAlpha01.py
@@ -1,4 +1,9 @@
 import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QtGui import QImage, QPalette, QBrush, QIcon
+from PyQt5.QtCore import Qt, QBasicTimer
+from PyQt5.QtGui import QPixmap
 FONTSIZE = 10
 
 #파일명만 바꿔서
